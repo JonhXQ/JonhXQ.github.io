49c4c6012ecd3d0fd9ca55b148be2fa34ccbf16b
JonhXQ <LopezJuarezJese.123@hotmail.com>
2023-10-24 19:09:36 -0600

    Web 11

--- IMDB/src/main.jsx
+++ IMDB/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 
-import './index.css'
+import { SearchProvider } from './components/SearchContext.jsx'
+
+/* Importación estilo CSS */
+import './style/index.css'
+
+/* Importación de Bootstrap CSS */
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+/* Importación de Bootstrap JS */
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
   </React.StrictMode>
 )
